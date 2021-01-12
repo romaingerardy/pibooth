77cbbe8d85550a8640e8a95dc1fffbb057f97c45
romaingerardy <rom.gerardy@gmail.com>
2021-01-12 21:38:12 +0100

    add plugin qrcode

--- external-plugins/pibooth-qrcode/pibooth_qrcode.py
+++ external-plugins/pibooth-qrcode/pibooth_qrcode.py
@@ -36,7 +36,7 @@ def state_wait_enter(app, win):
     if hasattr(app, 'previous_qr'):
         win_rect = win.get_rect()
         qr_rect = app.previous_qr.get_rect()
-        win.surface.blit(app.previous_qr, (10, win_rect.height - qr_rect.height - 10))
+        win.surface.blit(app.previous_qr, ((win_rect.height/2) - (qr_rect.width/2), win_rect.height - qr_rect.height - 10))
 
 
 @pibooth.hookimpl
@@ -71,5 +71,5 @@ def state_print_enter(app, win):
     """
     win_rect = win.get_rect()
     qr_rect = app.previous_qr.get_rect()
-    win.surface.blit(app.previous_qr, (0,
-                                       100))
+    win.surface.blit(app.previous_qr, (win_rect.width - qr_rect.width - 10,
+                                       win_rect.height - qr_rect.height - 10))
