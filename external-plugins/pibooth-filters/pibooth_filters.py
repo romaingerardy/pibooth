85b8cc847488c7b169ecd98b6cc7c0fef9f85fdc
romaingerardy <rom.gerardy@gmail.com>
2021-01-19 19:39:29 +0100

    Add filters (test)

--- external-plugins/pibooth-filters/pibooth_filters.py
+++ external-plugins/pibooth-filters/pibooth_filters.py
@@ -7,22 +7,27 @@ from pibooth.utils import timeit
 __version__ = "0.0.1"
 
 
-@pibooth.hookimpl
-def pibooth_configure(cfg):
-    """Declare the new configuration options"""
-    cfg.add_option('FILTERS', 'filters_list', "('GrayScale')", "Filters enabled")
+class FiltersPlugin(object):
+
+    def __init__(self):
+        print("Init FiltersPlugin")
+
+    @pibooth.hookimpl
+    def pibooth_configure(cfg):
+        """Declare the new configuration options"""
+        cfg.add_option('FILTERS', 'filters_list', "('GrayScale')", "Filters enabled")
 
 
-@pibooth.hookimpl
-def state_processing_do(self, cfg, app):
-    with timeit("Saving filtered raw captures"):
-        captures = app.camera.get_captures()
+    @pibooth.hookimpl
+    def state_processing_do(self, cfg, app):
+        with timeit("Saving filtered raw captures"):
+            captures = app.camera.get_captures()
 
-        for savedir in cfg.gettuple('GENERAL', 'directory', 'path'):
-            rawdir = osp.join(savedir, "raw", app.capture_date)
+            for savedir in cfg.gettuple('GENERAL', 'directory', 'path'):
+                rawdir = osp.join(savedir, "raw", app.capture_date)
 
-            for capture in captures:
-                count = captures.index(capture)
-                # img_path = osp.join(rawdir, "pibooth{:03}.jpg".format(count))
-                filter_controller.pilgram_aden(capture, osp.join(rawdir, "pibooth{:03}-aden.jpg".format(count)))
-                # filter_controller.im_color_temp(4000, img_path, osp.join(rawdir, "pibooth{:03}-filter.jpg".format(count)))
+                for capture in captures:
+                    count = captures.index(capture)
+                    # img_path = osp.join(rawdir, "pibooth{:03}.jpg".format(count))
+                    filter_controller.pilgram_aden(capture, osp.join(rawdir, "pibooth{:03}-aden.jpg".format(count)))
+                    # filter_controller.im_color_temp(4000, img_path, osp.join(rawdir, "pibooth{:03}-filter.jpg".format(count)))
