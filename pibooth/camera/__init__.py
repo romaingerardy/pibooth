45a57462dcedef50fe5925ffb7721c6be79d0bc1
romaingerardy <rom.gerardy@gmail.com>
2021-01-25 13:20:56 +0100

    Gtk

--- pibooth/camera/__init__.py
+++ pibooth/camera/__init__.py
@@ -2,10 +2,10 @@
 
 from pibooth.utils import LOGGER, pkill
 from pibooth.camera.rpi import RpiCamera, rpi_camera_connected
-from pibooth.camera.gphoto import GpCamera, gp_camera_connected
-from pibooth.camera.gphoto_omx import GpOmxCamera, gpomx_camera_connected
-from pibooth.camera.opencv import CvCamera, cv_camera_connected
-from pibooth.camera.hybrid import HybridRpiCamera, HybridCvCamera
+#from pibooth.camera.gphoto import GpCamera, gp_camera_connected
+#from pibooth.camera.gphoto_omx import GpOmxCamera, gpomx_camera_connected
+#from pibooth.camera.opencv import CvCamera, cv_camera_connected
+#from pibooth.camera.hybrid import HybridRpiCamera, HybridCvCamera
 
 
 def get_camera(iso, resolution, rotation, flip, delete_internal_memory):
@@ -15,28 +15,28 @@ def get_camera(iso, resolution, rotation, flip, delete_internal_memory):
     The priority order is chosen in order to have best rendering during preview
     and to take captures.
     """
-    if gp_camera_connected() and rpi_camera_connected():
-        LOGGER.info("Configuring hybrid camera (Picamera + gPhoto2) ...")
-        cam_class = HybridRpiCamera
-        pkill('*gphoto2*')
-    elif gp_camera_connected() and cv_camera_connected():
-        LOGGER.info("Configuring hybrid camera (OpenCV + gPhoto2) ...")
-        cam_class = HybridCvCamera
-        pkill('*gphoto2*')
-    elif gpomx_camera_connected():
-        LOGGER.info("Configuring gPhoto2 camera (preview with OMXPlayer) ...")
-        cam_class = GpOmxCamera
-        pkill('*gphoto2*')
-    elif gp_camera_connected():
-        LOGGER.info("Configuring gPhoto2 camera ...")
-        cam_class = GpCamera
-        pkill('*gphoto2*')
-    elif rpi_camera_connected():
+    #if gp_camera_connected() and rpi_camera_connected():
+    #    LOGGER.info("Configuring hybrid camera (Picamera + gPhoto2) ...")
+    #    cam_class = HybridRpiCamera
+    #    pkill('*gphoto2*')
+    #elif gp_camera_connected() and cv_camera_connected():
+    #    LOGGER.info("Configuring hybrid camera (OpenCV + gPhoto2) ...")
+    #    cam_class = HybridCvCamera
+    #    pkill('*gphoto2*')
+    #elif gpomx_camera_connected():
+    #    LOGGER.info("Configuring gPhoto2 camera (preview with OMXPlayer) ...")
+    #    cam_class = GpOmxCamera
+    #    pkill('*gphoto2*')
+    #elif gp_camera_connected():
+    #    LOGGER.info("Configuring gPhoto2 camera ...")
+    #    cam_class = GpCamera
+    #    pkill('*gphoto2*')
+    if rpi_camera_connected():
         LOGGER.info("Configuring Picamera camera ...")
         cam_class = RpiCamera
-    elif cv_camera_connected():
-        LOGGER.info("Configuring OpenCV camera ...")
-        cam_class = CvCamera
+    #elif cv_camera_connected():
+    #    LOGGER.info("Configuring OpenCV camera ...")
+    #    cam_class = CvCamera
     else:
         raise EnvironmentError("Neither Raspberry Pi nor GPhoto2 nor OpenCV camera detected")
 
