19e2a47efcdd1928b0281b74573af830c60e804e
romaingerardy <rom.gerardy@gmail.com>
2021-01-25 21:29:06 +0100

    Gtk

--- pibooth/view/scenes/processing.py
+++ pibooth/view/scenes/processing.py
@@ -3,8 +3,9 @@ from pgi import require_version
 from pibooth.view.scenes.paths import processing_media_path
 
 require_version('Gtk', '3.0')
+from pgi.repository import Gtk, GdkPixbuf
 
-from pibooth.view.scene import Scene
+from pibooth.view.scene import Scene, Placement
 from pibooth.utils import LOGGER
 
 
@@ -21,3 +22,11 @@ class ProcessingScene(Scene):
     def _setup(self):
         self.set_background(processing_media_path('BG-Processing.png'),
                             processing_media_path('BG-Processing.png'))
+
+        anim = GdkPixbuf.PixbufAnimation.new_from_file(processing_media_path('processing.gif'))
+
+        self.add_widget(
+            Gtk.Image.set_from_animation(anim),
+            Placement(0.5, 0.5, 1),
+            Placement(0.5, 0.5, 1)
+        )
