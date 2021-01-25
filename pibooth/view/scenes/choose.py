5f4bcf8dbffb3686dfce5f6712e4b010618bde22
romaingerardy <rom.gerardy@gmail.com>
2021-01-25 17:29:04 +0100

    Gtk

--- pibooth/view/scenes/choose.py
+++ pibooth/view/scenes/choose.py
@@ -1,10 +1,11 @@
 from pgi import require_version
 
-from pibooth.view.scenes.paths import common_media_path
+from pibooth.view.scenes.paths import common_media_path, choose_media_path
 
 require_version('Gtk', '3.0')
+from pgi.repository import Gtk, GLib, GObject, Gdk
 
-from pibooth.view.scene import Scene
+from pibooth.view.scene import Scene, Placement
 from pibooth.utils import LOGGER
 
 
@@ -22,6 +23,15 @@ class ChooseScene(Scene):
                             common_media_path('BG-Blank.png'))
 
         # Add choices
-        if choices & len(choices) == 2:
+        if choices and len(choices) == 2:
             LOGGER.info(choices[0])
             LOGGER.info(choices[1])
+
+            x = 0.3
+            for choice in choices:
+                self.add_widget(
+                    Gtk.Image.new_from_file(choose_media_path('choice_po_' + choice + '.png')),
+                    Placement(x, 0.5, 1),
+                    Placement(x, 0.5, 1)
+                )
+                x = x + 0.4
