366424adfbe7183a2ea8e4b79ce8fbc4f42ba263
romaingerardy <rom.gerardy@gmail.com>
2021-01-28 21:20:09 +0100

    config

--- pibooth/view/scenes/configuration.py
+++ pibooth/view/scenes/configuration.py
@@ -22,8 +22,8 @@ class ConfigurationScene(Scene):
         # Back icon
         self.add_widget(
             Gtk.Image.new_from_file(common_media_path('back-icon.png')),
-            Placement(0.3, 0.3, 1),
-            Placement(0.3, 0.3, 1),
+            Placement(0.05, 0.05, 1),
+            Placement(0.05, 0.05, 1),
             self._back
         )
 
