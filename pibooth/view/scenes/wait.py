13f9abc73ef0f2341ad45d40666ba8f980e1ee95
romaingerardy <rom.gerardy@gmail.com>
2021-01-29 15:35:48 +0100

    captures

--- pibooth/view/scenes/wait.py
+++ pibooth/view/scenes/wait.py
@@ -27,21 +27,22 @@ class WaitScene(Scene):
         # Add the transparent image = the touch area to start the shooting
         # Will be transformed to a transparent button
         # TODO to fix
-        self.add_widget(
-            Gtk.Image.new_from_file(wait_media_path('touch_area.png')),
-            Placement(0, 0, 1),
-            Placement(0, 0, 1),
-            clicked_cb=self._nextStep,
-            key=None,
-            name=None,
-            modal=False,
-            transparent=True
-        )
+        #self.add_widget(
+        #    Gtk.Image.new_from_file(wait_media_path('touch_area.png')),
+        #    Placement(0, 0, 1),
+        #    Placement(0, 0, 1),
+        #    clicked_cb=self._nextStep,
+        #    key=None,
+        #    name=None,
+        #    modal=False,
+        #    transparent=True
+        #)
 
         self.add_widget(
             Gtk.Image.new_from_file(wait_media_path('wifi_icon.png')),
             Placement(0.7, 0.9, 1),
-            Placement(0.7, 0.9, 1)
+            Placement(0.7, 0.9, 1),
+            self._nextStep()
         )
 
         self.add_widget(
